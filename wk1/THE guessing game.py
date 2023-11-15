10f1bd673d16644b734a2a9bbc406094ac30dcf4
o.ayeniCSC101 <omotayo.ayeni@pau.edu.ng>
2023-11-15 11:24:53 +0100

    (The Guessing Game)

---
+++ wk1/THE guessing game.py
@@ -0,0 +1,24 @@
+import random
+
+def get_random_number():
+    return random.randint(1, 10)
+
+def guess_number(number):
+    while True:
+        try:
+            guess = int(input("Guess the number between 1 and 10: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if guess < number:
+            print("Too low! Try again.")
+        elif guess > number:
+            print("Too high! Try again.")
+        else:
+            print("Congratulations! You guessed the correct number.")
+            break
+
+if __name__ == "__main__":
+    random_number = get_random_number()
+    guess_number(random_number)
